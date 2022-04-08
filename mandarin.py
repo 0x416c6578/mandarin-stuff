cb5d177ed6d4fd9d6b39b5e3e9617d375027a8a4
0x416c6578 <14030169+0x416c6578@users.noreply.github.com>
2022-04-08 21:59:23 +0100

    Updated with simple testing

--- mandarin.py
+++ mandarin.py
@@ -3,9 +3,19 @@ import csv
 import sys
 import random
 
-# Currently doesn't do much...
+strMandarin = "mandarin"
+strPinyinWithTones = "pinyin_tones"
+strPinyinWithoutTones = "pinyin_no_tones"
+strTones = "tones"
+strDefinition = "definition"
+strLearningAid = "learning_aid"
+
 with open(sys.argv[1], mode="r") as mandarinFile:
     csvDictReader = csv.DictReader(mandarinFile)
-    for i in list(csvDictReader):
-        print(f"{i}")
-    # print(len(list(csvDictReader)))
+    vocab = list(csvDictReader)
+    random.shuffle(vocab)
+    numWords = len(vocab)
+    print("Press enter for next word")
+    for word in vocab:
+      print("Pinyin: " + word[strPinyinWithoutTones] + " (" + word[strDefinition] + ")", end="")
+      input("")
