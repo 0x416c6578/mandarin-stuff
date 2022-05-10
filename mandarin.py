ec3b967ad5b467535a26b7938458e78749e8f3f1
0x416c6578 <14030169+0x416c6578@users.noreply.github.com>
2022-05-10 22:48:26 +0100

    Started work on examples functionality

--- mandarin.py
+++ mandarin.py
@@ -9,14 +9,34 @@ strPinyinWithoutTones = "pinyin_no_tones"
 strTones = "tones"
 strDefinition = "definition"
 strLearningAid = "learning_aid"
+strExample = "example"
 
 with open(sys.argv[1], mode="r") as mandarinFile:
     csvDictReader = csv.DictReader(mandarinFile)
     vocab = list(csvDictReader)
     random.shuffle(vocab)
     numWords = len(vocab)
-    print("Press enter for next word")
+    print(
+        """
+---------------------------------
+enter for next word
+\"h\" enter for learning aid
+\"q\" enter to exit
+---------------------------------"""
+    )
     for word in vocab:
-      print("Pinyin: " + word[strPinyinWithoutTones] + " (" + word[strDefinition] + ")", end="")
-      input("")
-      print("Answer: " + word[strMandarin])
+        print(
+            f"\nPinyin (no tones): {word[strPinyinWithoutTones]} ({word[strDefinition]})  ",
+            end="",
+        )
+        bruh = input("")
+        if "q" in bruh:
+            print("再见！")
+            sys.exit(0)
+        if "h" in bruh:
+            if len(word[strLearningAid]) > 0:
+                print(f"  {word[strLearningAid]}  ", end="")
+            else:
+                print(f"  No learning aid available  ", end="")
+            input("")
+        print(f"  Answer: {word[strMandarin]}, example: {word[strExample]}")
