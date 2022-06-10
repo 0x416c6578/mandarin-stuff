f424faf17baef051acbe03368a8f69317784517d
0x416c6578 <14030169+0x416c6578@users.noreply.github.com>
2022-06-10 16:53:54 +0100

    Added alternative testing mode

--- mandarin.py
+++ mandarin.py
@@ -5,7 +5,9 @@ import random
 import argparse
 import os
 import glob
+import time
 
+# String constants
 strMandarin = "mandarin"
 strPinyinWithTones = "pinyin_tones"
 strPinyinWithoutTones = "pinyin_no_tones"
@@ -14,6 +16,9 @@ strDefinition = "definition"
 strLearningAid = "learning_aid"
 strExample = "example"
 
+vocab = None
+
+# Argument parsing
 parser = argparse.ArgumentParser(description="Learn Mandarin")
 parser.add_argument(
     "fileOrDir", type=str, help="file or directory of files to test with"
@@ -24,8 +29,23 @@ parser.add_argument(
     action="store_true",
     help="use a directory of csv files -> will test using all files",
 )
+
+parser.add_argument(
+    "-c",
+    "--characterTest",
+    action="store_true",
+    help="tests pinyin (no tones) + translation -> character",
+)
+
+parser.add_argument(
+    "-p",
+    "--pinyinTest",
+    action="store_true",
+    help="tests character -> pinyin + tones + translation",
+)
 args = parser.parse_args()
 
+# Load vocabulary from file(s)
 path = args.fileOrDir
 if args.useDirectory:
     if not os.path.isdir(path):
@@ -44,41 +64,62 @@ for file in files:
         csvDictReader = csv.DictReader(f)
         vocab += list(csvDictReader)
 
+# Shuffle vocab list
 random.shuffle(vocab)
+print("Mandarin learning aid program")
 
-print(
-    """
----------------------------------
-Enter for next word
-h+Enter for learning aid
-q+Enter to exit
----------------------------------"""
-)
-
-numWords = len(vocab)
-i = 1
-for word in vocab:
-    print(
-        f"\n---------------- {i}/{numWords} ----------------\n"
-        f"Pinyin (no tones): {word[strPinyinWithoutTones]} ({word[strDefinition]})  ",
-        end="",
-    )
-    inp = input("")
-    if "q" in inp:
-        print("再见！")
-        sys.exit(0)
-    if "h" in inp:
-        if len(word[strLearningAid]) > 0:
-            print(f"  {word[strLearningAid]}  ", end="")
+if not args.pinyinTest:
+    # Test pinyin (no tones) + translation -> character
+    print("Pinyin to character testing (default)")
+    numWords = len(vocab)
+    i = 1
+    for word in vocab:
+        print(
+            f"\n---------------- {i}/{numWords} ----------------\n"
+            f"Pinyin (no tones): {word[strPinyinWithoutTones]} ({word[strDefinition]})  ",
+            end="",
+        )
+        inp = input("")
+        if "q" in inp:
+            print("再见！")
+            sys.exit(0)
+        if "h" in inp:
+            if len(word[strLearningAid]) > 0:
+                print(f"  {word[strLearningAid]}  ", end="")
+            else:
+                print(f"  No learning aid available  ", end="")
+            input("")
+        if word[strExample] != "":
+            example = "  Example: " + word[strExample] + "\n"
         else:
-            print(f"  No learning aid available  ", end="")
-        input("")
-    if word[strExample] != "":
-        example = "\n  Example: " + word[strExample]
-    else:
-        example = ""
-    print(
-        f"  Answer: {word[strMandarin]}\n  Full pinyin: {word[strPinyinWithTones]} ({word[strTones]})"
-        + example
-    )
-    i += 1
+            example = "\n"
+
+        print(f"  Answer: {word[strMandarin]}")
+        time.sleep(0.12)
+        print(f"  Full pinyin: {word[strPinyinWithTones]} ({word[strTones]})")
+        time.sleep(0.12)
+        print(example, end="")
+        time.sleep(0.12)
+        i += 1
+
+elif not args.characterTest and args.pinyinTest:
+    # Test character -> pinyin + tones + translation
+    print("Character to pinyin testing (from -p flag)")
+    numWords = len(vocab)
+    i = 1
+    for word in vocab:
+        print(
+            f"\n---------------- {i}/{numWords} ----------------\n"
+            f"Character: {word[strMandarin]}   ",
+            end="",
+        )
+        inp = input("")
+        if "q" in inp:
+            print("再见！")
+            sys.exit(0)
+
+        print(f"  Answer: {word[strPinyinWithTones]} ({word[strTones]})")
+        time.sleep(0.12)
+        print(f"  Translation: {word[strDefinition]}")
+        time.sleep(0.12)
+        i += 1
